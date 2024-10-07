5fd055d6afcbd2bec0c16022ef6a93a83018761b
Frank <frank.snelling03@gmail.com>
2024-10-07 18:29:49 +0100

    load dataset

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import tensorflow
-import keras
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
+
+# load in dataset CIFAR-10 to train image detection
+(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+
+print(x_train)
